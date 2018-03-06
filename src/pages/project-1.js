cc29de7ce91b61ce0cabd25f6e44e3179dfadf42
Hunter Caron <huntercaron@mac.com>
2018-03-06 12:32:33 -0500

    tweaks and new icons

--- src/pages/project-1.js
+++ src/pages/project-1.js
@@ -6,6 +6,7 @@ import dat from 'dat.gui';
 import angleIcon from '../assets/images/angle-icon.svg'
 import petalIcon from '../assets/images/petal-icon.svg'
 import petalLengthIcon from '../assets/images/petal-length-icon.svg'
+import petalWidthIcon from '../assets/images/petal-width-icon.svg'
 
 import sketch from '../sketches/output/phone-vis_dual-flipped-animated'
 import phoneRecords from '../sketches/assets/phoneRecords.json'
@@ -24,8 +25,12 @@ const SketchContainer = styled.div`
 `
 
 const Title = styled.h1`
+  margin: 0;
   font-size: ${fontSize};
-  opacity: 0.75;
+
+  span {
+    opacity: 0.75;
+  }
 `
 
 const SubTitle = styled.h4`
@@ -40,26 +45,32 @@ const Info = styled.div`
 `;
 
 const Icon = styled.img`
-
+  width: 50px;
 `
 
 const LegendItemContainer = styled.div`
   display: flex;
   color: white;
   opacity: 0.75;
+  align-items: center;
+  min-width: 150px;
 
   p {
     font-size: ${fontSize};
+    margin: 0;
     margin-left: 2rem;
+    margin-top: 5px;
+    font-size: 1.2rem;
   }
 `
 
 const Legend = styled.div`
-  position: absolute;
   bottom: 2rem;
+  position: absolute;
   width: 100%;
   display: flex;
   justify-content: space-around;
+  flex-wrap: wrap;
 `;
 
 
@@ -76,7 +87,7 @@ function IamLegend(props) {
 // page component
 export default class Project1 extends React.Component {
   componentDidMount() {
-    this.gui = new dat.GUI();
+    // this.gui = new dat.GUI();
     var myp5 = new p5(sketch, this.mount);
     
     
@@ -90,13 +101,14 @@ export default class Project1 extends React.Component {
         </SketchContainer>
 
         <Info>
-          <Title>Together: Visualizing Relationships</Title>
+          <Title>Together: <span>Visualizing Relationships</span></Title>
         </Info>
         
         <Legend>
           <IamLegend icon={angleIcon} text={"ANGLE: UNIQUE PHONE #"} />
           <IamLegend icon={petalIcon} text={"PETAL: PHONE-CALL"} />
-          <IamLegend icon={petalLengthIcon} text={"LENGTH: CALL-LENGTH"} />
+          <IamLegend icon={petalLengthIcon} text={"LENGTH & COLOUR: CALL-LENGTH"} />
+          <IamLegend icon={petalWidthIcon} text={"WIDTH: TIME OF DAY (0h-24h)"} />
         </Legend>
       </Container>
     )
@@ -111,175 +123,3 @@ export default class Project1 extends React.Component {
 //     }
 //   }
 // `
-
-var proj1Sketch = function (sketch)  {
-  const records = phoneRecords;
-  console.log("WASSAAHH");
-  
-
-  // helper functions
-  const uniqueArray = arr => [...new Set(arr)];
-  const propArray = (data, value) => data.map(node => node[value]);
-  const uniqueObj = (data, value) => uniqueArray(propArray(data, value));
-
-  let n = 0;
-  let curve;
-  let revCurve;
-
-  let lineY;
-  let radius;
-  let numPoints;
-  let angle;
-  let leaves = [];
-
-  let animFrames = 30;
-  let currentNum = 0;
-
-  const scaleMultiplier = 0.01;
-  const scaleBase = 0.1;
-
-  const maxLength = Math.max(...propArray(records, 'length'));
-  let totalLengths;
-  let maxTotalLength;
-  let minTotalLength;
-  let maxCalls;
-
-  class Leaf {
-    constructor(number, calls) {
-      this.number = number;
-      this.calls = calls;
-      this.radius = 0;
-    }
-
-    draw = () => {
-
-
-      let amp = 120;
-      curve = 1 * amp;
-      revCurve = 1 * -amp;
-
-      // Stroke Drawing
-      sketch.stroke(
-        sketch.map(this.calls.length, 1, maxLength, 79, 35),
-        sketch.map(this.calls.length, 1, maxLength, 104, 200),
-        sketch.map(this.calls.length, 1, maxLength, 250, 100),
-        120
-      );
-
-      sketch.strokeWeight(0.6);
-      sketch.noFill();
-      sketch.push();
-
-      let callLengths = propArray(this.calls, "length");
-      let totalMinutes = callLengths.reduce((a, b) => a + b);
-      // propArray(this.calls, "length")
-      // console.log(totalMinutes);
-
-
-      let scale = (totalMinutes * 0.01 + 0.1);
-      // console.log(maxCalls);
-
-      // console.log(sketch.map(this.calls.length, 0, maxCalls, 0, 1));
-      sketch.scale(sketch.map(this.calls.length, 0, maxCalls, 0, 0.6) + 0.9);
-
-      // sketch.beginShape();
-      // sketch.bezier(0, 0, radius / 4, 0 - curve, radius / 4 * 3, 0 - curve, radius, 0);
-      // sketch.bezier(0, 0, radius / 4, 0 - revCurve, radius / 4 * 3, 0 - revCurve, radius, 0);
-      // sketch.endShape(sketch.CLOSE);
-      sketch.pop();
-
-      for (let call in this.calls) {
-        this.drawCall(this.calls[call], call);
-      }
-    }
-
-    drawCall = (call, index) => {
-      let time = call.time.split(':');
-      let minutes = (+time[0]) * 60 + (+time[1]);
-
-      let amp = 120;
-      curve = (minutes / 1440) * amp;
-      revCurve = (minutes / 1440) * -amp;
-
-
-
-      // fill drawing
-      sketch.fill(
-        sketch.map(call.length, 1, maxLength, 79, 35),
-        sketch.map(call.length, 1, maxLength, 104, 200),
-        sketch.map(call.length, 1, maxLength, 250, 100),
-        20
-      );
-
-
-
-      sketch.noStroke();
-      sketch.push();
-      let scale = (sketch.map(call.length, 1, maxLength, 0, 0.6) + 0.9);
-      // let animThisFrames = 100+(this.calls.length/8*80);
-      let animThisFrames = 200 - call.length * 2;
-      let animBaseFrames = index * 20;
-      let mappedScale = sketch.map(sketch.frameCount, animBaseFrames, animThisFrames + animBaseFrames, 0, scale)
-      if (mappedScale < 0)
-        mappedScale = 0;
-
-      sketch.scale((sketch.frameCount < animThisFrames + animBaseFrames) ? mappedScale : scale);
-
-      sketch.beginShape();
-      sketch.bezier(0, 0, radius / 4, 0 - curve, radius / 4 * 3, 0 - curve, radius, 0);
-      sketch.bezier(0, 0, radius / 4, 0 - revCurve, radius / 4 * 3, 0 - revCurve, radius, 0);
-      sketch.endShape(sketch.CLOSE);
-      sketch.pop();
-    }
-
-    logInfo = () => {
-      // console.log(this.number, this.calls);
-    }
-  }
-
-
-  function setup() {
-    createCanvas(windowWidth, windowHeight);
-    height = windowHeight;
-    angleMode(DEGREES);
-    background(0);
-
-    for (let phoneNum of uniqueObj(records, 'phoneNum')) {
-      if (records.filter(r => r.phoneNum === phoneNum)) {
-        leaves.push(new Leaf(phoneNum, records.filter(r => r.phoneNum === phoneNum)))
-      }
-    }
-
-    totalLengths = leaves.map(node => propArray(node["calls"], "length").reduce((a, b) => a + b));
-    maxTotalLength = Math.max(...totalLengths);
-    minTotalLength = Math.min(...totalLengths);
-    maxCalls = Math.max(...leaves.map(node => node.calls.length));
-
-    noFill();
-    stroke(200);
-
-
-    lineY = height / 2;
-    radius = height * 0.4;
-    numPoints = 32;
-    angle = TWO_PI / numPoints;
-  };
-
-
-  function draw() {
-    currentNum = frameCount / animFrames;
-    background(0);
-
-    translate(width / 2, height / 2);
-
-    for (let leaf of leaves) {
-      rotate(360 / leaves.length);
-      leaf.draw();
-    }
-
-    if (currentNum > 50) {
-      noLoop();
-    }
-  };
-
-}
